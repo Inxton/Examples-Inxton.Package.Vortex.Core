b178a703d3d04c26308dbdeb1f1f5b382cb1fafe
Jozef Chmelar ml <jozef.chmelar.ml@mts.sk>
2020-03-16 14:46:50 +0100

    Local is the default PLC Removed unnecessary comments Connector is not the startup project but Weather.Wpf is.

--- HansPlcConnector/Entry.cs
+++ HansPlcConnector/Entry.cs
@@ -1,41 +1,18 @@
-﻿#define REMOTE // Comment if your target is localhost
+﻿#define LOCAL // Comment if your target is remote
 
-using HansPlc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Vortex.Adapters.Connector.Tc3.Adapter;
 
 namespace HansPlc
 {
     public static class Entry
     {
-#if REMOTE
-        /// <summary>
-        /// Target system ams id.
-        /// </summary>
-        const string AmsId = "172.20.10.102.1.1"; // your ams id or set to 'null' if local
-
-        /// <summary>
-        /// Port of the target system.
-        /// </summary>
+#if LOCAL
+        const string AmsId = null; // your ams id or set to 'null' if local
         const int Port = 851;
 #else
-        /// <summary>
-        /// Target system ams id. (local)
-        /// </summary>
-        const string AmsId = null
-
-        /// <summary>
-        /// Port of the target system.
-        /// </summary>
+        const string AmsId = "172.20.10.102.1.1";
         const int Port = 851;
-
 #endif
-
         public static HansPlcTwinController HansPlc { get; } = new HansPlcTwinController(Tc3ConnectorAdapter.Create(AmsId, Port));
-
     }
 }
